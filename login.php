3472c6cbb4edca1df91328850a81be9cd72da4b9
yamadakanoko <58495214+yamadakanoko@users.noreply.github.com>
2019-12-11 10:07:09 +0900

    Update login.php

--- login.php
+++ login.php
@@ -3,27 +3,72 @@
 <head>
   <meta charset="UTF-8" />
   <link rel="stylesheet" href="ectest.css">
-  <script type="text/javascript" src=""></script>
+  <?php
+  session_start();
+// データベースに接続するために必要なデータソースを変数に格納
+  // mysql:host=ホスト名;dbname=データベース名;charset=文字エンコード
+$dsn = 'mysql:host=localhost;dbname=sumple;charset=utf8';
+  // データベースのユーザー名
+$user = 'root';
+  // データベースのパスワード
+$password = '';
+// tryにPDOの処理を記
+try {
+  // PDOインスタンスを生成
+  $dbh = new PDO($dsn, $user, $password);
+// エラー（例外）が発生した時の処理を記述
+} catch (PDOException $e) {
+  // エラーメッセージを表示させる
+  echo 'データベースにアクセスできません！' . $e->getMessage();
+  // 強制終了
+  exit;
+}
+// SELECT文を変数に格納
+$sql = "SELECT * FROM youseracount";
+// SQLステートメントを実行し、結果を変数に格納
+$stmt = $dbh->query($sql);
+// foreach文で配列の中身を一行ずつ出力
+$errorMessage = '';
+foreach ($stmt as $row) {
+  // ログインボタンを押したときの動作
+  if(isset($_POST['login'])){
+  if($_POST['account'] == $row['account'] && $_POST['password'] == $row['password']){
+    header('location: itemitiran.html');
+    exit;
+  }elseif (empty($_POST["account"]) && empty($_POST["password"])) {
+    $errorMessage = "ログインIDとパスワードが未入力です。";
+  } else if (empty($_POST["password"])) {
+    $errorMessage = "パスワードが未入力です。";
+  } else if (empty($_POST["account"])) {
+    $errorMessage = "ログインIDが未入力です。";
+  }else{
+    $errorMessage = 'ログインIDまたはパスワードが間違っています。';
+  }
+}
+}
+
+?>
   <title>マスターメンテナンスログイン</title>
 </head>
 <body>
-  <form action="yamada.php" method="POST">
   <header>
-<h1>Drag Takayama</h1>
-<a href="" class="frontlink">フロントエンド</a>
-</header>
-<div class="login">
-  <p>DragStoreマスターメンテナンス</p>
-<p>
-  ログインID
-<input type="text" name="roginid">
-</p>
-<p>
-  パスワード
-  <input type="text" name="password">
-</p>
-<input type="submit" value="ログイン" class="loginbutton">
-</div>
-</form>
+    <h1>Drag Takayama</h1>
+    <a href="" class="frontlink">フロントエンド</a>
+  </header>
+  <div class="login">
+    <p>DragStoreマスターメンテナンス</p>
+    <form action="" method="POST">
+      <p class="error"><?php echo $errorMessage ?></p>
+      <p>
+        ログインID
+        <input type="text" name="account" value="">
+      </p>
+      <p>
+        パスワード
+        <input type="password" name="password" value="">
+      </p>
+      <input type="submit" name="login" value="ログイン" class="loginbutton">
+    </form>
+  </div>
 </body>
 </html>
