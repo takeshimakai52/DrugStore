a4b1286e4548d6a88feb221a3f85f1fe2c3e8fa7
yamadakanoko <58495214+yamadakanoko@users.noreply.github.com>
2019-12-26 16:38:04 +0900

    フロントエンドの見た目です

---
+++ fe_headAndSide.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8" />
+	<title>DrugStore</title>
+	<link rel="stylesheet" type="text/css" href="fe_style.css">
+</head>
+<body>
+	<header>
+		<div class="title">
+			<a name="topLink" href="#">
+				<h1>DrugStore</h1>
+			</a>
+		</div>
+		<div class="search">
+			<input type="text" name="searchBox_input">
+			<input type="button" name="searchButton_button" value="検索">
+		</div>
+		<div class="login">
+		<a href="">ログイン</a>
+	</div>
+	</header>
+</div>
+<div class="div_sideMenu">
+		<p class="h_sideMenu">ジャンル</p>
+		<ul>
+			<a href="">
+				<li>
+					健康食品
+				</li>
+			</a>
+			<a href="">
+				<li>
+					コスメ
+				</li>
+			</a>
+			<a href="">
+				<li>
+					ビタミン
+				</li>
+			</a>
+			<a href="">
+				<li>
+					ミネラル
+				</li>
+			</a>
+		</ul>
+		<p class="h_sideMenu">メーカー</p>
+		<ul>
+		<a href="">
+			<li>
+				DHC
+			</li>
+		</a>
+		<a href="">
+			<li>
+				大塚製薬
+			</li>
+		</a>
+		<a href="">
+			<li>
+				ファンケル
+			</li>
+		</a>
+	</ul>
+		<p class="h_sideMenu">ブランド</p>
+		<ul>
+		<a href="">
+			<li>
+				DHC
+			</li>
+		</a>
+		<a href="">
+			<li>
+				FANCL
+			</li>
+		</a>
+		<a href="">
+			<li>
+				ULOS
+			</li>
+		</a>
+		<a href="">
+			<li>
+				インナーシグナル
+			</li>
+		</a>
+		<a href="">
+			<li>
+				大塚製薬
+			</li>
+		</a>
+		<a href="">
+			<li>
+				カロリーメイト
+			</li>
+		</a>
+		<a href="">
+			<li>
+				ネイチャーメイド
+			</li>
+		</a>
+	</ul>
+</div>
+<div>
+	<p class="ichiranTitle">
+		商品一覧
+	</p>
+</div>
+</body>
+</html>
